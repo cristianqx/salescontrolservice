d84aab0590f28464ef09f478aa761b11fef931bb
Cristian Baptistella <cbaptistella@magnasistemas.com.br>
2020-08-26 10:15:28 -0300

    initial

---
+++ src/main/java/br/com/salescontrolservice/SalescontrolserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.salescontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalescontrolserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalescontrolserviceApplication.class, args);
+	}
+
+}
